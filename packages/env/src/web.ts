5b6d6939ef37e980b727638b966b4e75c181afcd
Swargaraj Bhowmik <swargarajbhowmik@gmail.com>
2026-07-02 21:48:32 +0530

    refactor: remove unused env package

--- packages/env/src/web.ts
+++
@@ -1,7 +0,0 @@
-import { createEnv } from "@t3-oss/env-core";
-import { z } from "zod";
-
-export const env = createEnv({
-  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
-  emptyStringAsUndefined: true,
-});
